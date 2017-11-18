b3691768274de3cbfb5e863a769ad96cb97c5caf
Valerie Kraucunas <valeriekraucunas@gmail.com>
2017-11-17 18:31:26 -0700

    router configured

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import Logo from './logo.svg';
+import { Link, Route, Switch } from 'react-router-dom';
+import BarbiesDisplay from './pages/BarbiesDisplay'
+import BarbieDetail from './pages/BarbieDetail'
+import AddEditDoll from './pages/AddEditDoll'
+
+
 import './App.css';
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+/* Category component */
+const Category = () => (
+  <div>
+    <Switch>
+      <Route path="/" exact component={BarbiesDisplay} />
+      <Route path="/add" exact component={AddEditDoll} />
+      <Route path="/:id" component={BarbieDetail} />
+    </Switch>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Logo height="80px" width="80px" />
-          <h1 className="App-title">Welcome to React</h1>
+          <ul className="nav navbar-nav">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/barbies">Barbies</Link></li>
+            <li><Link to="/barbies/add">Add A Doll</Link></li>
+            <li><Link to="barbies/2">Skipper</Link></li>
+          </ul>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/barbies" component={Category}/>
+          </Switch>
+        </main>
       </div>
     );
   }
