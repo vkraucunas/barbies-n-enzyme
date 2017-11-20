272005a3e078e3f65a02da7efecdb7e875c4df39
Valerie Kraucunas <valeriekraucunas@gmail.com>
2017-11-20 10:43:58 -0700

    barbiesDisplay cleanup

--- src/pages/BarbiesDisplay/index.jsx
+++ src/pages/BarbiesDisplay/index.jsx
@@ -4,62 +4,6 @@ import {connect} from 'react-redux'
 import {getDolls} from '../../redux/actions'
 import BarbiesContainer from '../../components/BarbiesContainer'
 
-const BarbieInfo = [
-  {
-    id: "8",
-    name: "Barbie AF",
-    doll_type: "Barbie",
-    est_value: "$50",
-    photo_1: "https://media1.tenor.co/images/572cd9ee8ce078a5e04fb7bc493f24be/tenor.gif?itemid=5469934",
-    year_produced: "1998",
-    condition_id: "3",
-  },
-  {
-    id: "3",
-    name: "Barbie AF",
-    doll_type: "Barbie",
-    est_value: "$50",
-    photo_1: "https://media1.tenor.co/images/572cd9ee8ce078a5e04fb7bc493f24be/tenor.gif?itemid=5469934",
-    year_produced: "1998",
-    condition_id: "3",
-  },
-  {
-    id: "5",
-    name: "Barbie AF",
-    doll_type: "Barbie",
-    est_value: "$50",
-    photo_1: "https://media1.tenor.co/images/572cd9ee8ce078a5e04fb7bc493f24be/tenor.gif?itemid=5469934",
-    year_produced: "1998",
-    condition_id: "3",
-  },
-  {
-    id: "2",
-    name: "Barbie AF",
-    doll_type: "Barbie",
-    est_value: "$50",
-    photo_1: "https://media1.tenor.co/images/572cd9ee8ce078a5e04fb7bc493f24be/tenor.gif?itemid=5469934",
-    year_produced: "1998",
-    condition_id: "3",
-  },
-  {
-    id: "4",
-    name: "Barbie AF",
-    doll_type: "Barbie",
-    est_value: "$50",
-    photo_1: "https://media1.tenor.co/images/572cd9ee8ce078a5e04fb7bc493f24be/tenor.gif?itemid=5469934",
-    year_produced: "1998",
-    condition_id: "3",
-  },
-  {
-    id: "6",
-    name: "Barbie AF",
-    doll_type: "Barbie",
-    est_value: "$50",
-    photo_1: "https://media1.tenor.co/images/572cd9ee8ce078a5e04fb7bc493f24be/tenor.gif?itemid=5469934",
-    year_produced: "1998",
-    condition_id: "3",
-  }
-]
 class BarbiesDisplay extends Component {
 
   constructor(props) {
@@ -76,16 +20,11 @@ class BarbiesDisplay extends Component {
 
 
   render() {
-    if (this.props.dolls) {
-      console.log('====================================');
-      console.log(this.props.dolls);
-      console.log('====================================');
-      return (
-        <div>
-          <BarbiesContainer barbies={this.props.dolls} />
-        </div>
-      )
-    }
+    return (
+      <div>
+        <BarbiesContainer barbies={this.props.dolls} />
+      </div>
+    )
   }
 }
 
